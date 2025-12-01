291e2dea84da1c505fed059fa07dac4d7d2ea2df
Sergey Medvedev <medsv@yandex.ru>
2025-12-01 22:48:16 +0300

    Пропускная способность

--- pages/3_Пропускная_способность.py
+++ pages/3_Пропускная_способность.py
@@ -2,6 +2,16 @@ import streamlit as st
 #from math import pi
 from common.print_result import print_result
 from common.streamlit_components import create_unit_input, get_si_value, is_mass_flow
+
+st.set_page_config(
+    page_title="Пропускная способность"
+)
+
+st.title("Пропускная способность")
+
+with st.expander("Теория"):
+    st.markdown("[Статья](https://dzen.ru/a/XzvV9MLXtVfwVFUh) Что такое Kv и как его использовать")
+
 dens_v, dens_u = create_unit_input(
     label="Плотность среды",
     category="density",
@@ -15,13 +25,13 @@ dp_v, dp_u = create_unit_input(
     label="Потери давления",
     category="pressure",
     key="p",
-    value=100000.,
+    value=0.1,
     step=1.,
-    unit="Па"
+    unit="МПа"
     )
 dp: float = get_si_value(dp_v, dp_u, "pressure")
 
-Q_v, Q_u = create_unit_input(
+Kv_v, Kv_u = create_unit_input(
         label="Пропускная способность",
         category="volume_flow",
         key="Q",
@@ -29,6 +39,11 @@ Q_v, Q_u = create_unit_input(
         step=1.,
         unit="м³/ч"
         )
+Kv: float = get_si_value(Kv_v, Kv_u, "volume_flow")  # м3/c
+Kv *= 3600.  #м3/ч
 
 if st.button("Рассчитать"):
-    pass
+    Q = Kv * (dp / 100 / dens) ** 0.5
+    data = {"Пропускная способность, м3/ч": Kv, "Потери давления, Па": dp, 
+            "Плотность среды, кг/м3": dens, "Расход среды, м3/ч": Q, "Расход среды, м3/c": Q / 3600.}
+    print_result(data)
